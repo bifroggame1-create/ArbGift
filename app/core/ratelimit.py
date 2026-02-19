f53586e14811d50c018749410686e25ffd096720
bifroggame1-create <bifroggame1@gmail.com>
2026-02-19 12:40:38 +0300

    Fix slowapi config when .env missing in containers

--- app/core/ratelimit.py
+++ app/core/ratelimit.py
@@ -1,4 +1,8 @@
+import os
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from starlette.config import Config as StarletteConfig
 
-limiter = Limiter(key_func=get_remote_address)
+# Avoid starlette Config trying to read missing .env in container
+_config = StarletteConfig(env_file=None, environ=os.environ)
+limiter = Limiter(key_func=get_remote_address, config=_config)
--- services/solo-games/app/core/ratelimit.py
+++ services/solo-games/app/core/ratelimit.py
@@ -1,4 +1,7 @@
+import os
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from starlette.config import Config as StarletteConfig
 
-limiter = Limiter(key_func=get_remote_address)
+_config = StarletteConfig(env_file=None, environ=os.environ)
+limiter = Limiter(key_func=get_remote_address, config=_config)
