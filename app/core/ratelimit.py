e9203e873f3c2f47e78aa0530bc78134863673d4
bifroggame1-create <bifroggame1@gmail.com>
2026-02-20 16:15:54 +0300

    Make slowapi tolerant to missing .env

--- app/core/ratelimit.py
+++ app/core/ratelimit.py
@@ -1,8 +1,9 @@
 import os
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from starlette.config import Config as StarletteConfig
 
-# Avoid starlette Config trying to read missing .env in container
-_config = StarletteConfig(env_file=None, environ=os.environ)
-limiter = Limiter(key_func=get_remote_address, config=_config)
+# Ensure empty .env exists so SlowAPI/Starlette Config does not fail
+if not os.path.exists('.env'):
+    open('.env', 'a').close()
+
+limiter = Limiter(key_func=get_remote_address)
--- services/stars/app/core/ratelimit.py
+++ services/stars/app/core/ratelimit.py
@@ -1,7 +1,8 @@
 import os
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from starlette.config import Config as StarletteConfig
 
-_config = StarletteConfig(env_file=None, environ=os.environ)
-limiter = Limiter(key_func=get_remote_address, config=_config)
+if not os.path.exists('.env'):
+    open('.env', 'a').close()
+
+limiter = Limiter(key_func=get_remote_address)
